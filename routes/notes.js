7dbee370fb2a291753391a1e77f58e727f5c269b
Caitlin Stevenson <caitlinstevenson@Caitlins-Air.attlocal.net>
2022-04-16 05:14:30 -0500

    updated server.js, as well as index and notes routes.

---
+++ routes/notes.js
@@ -0,0 +1,41 @@
+const note = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all notes
+note.get('/', (req, res) => {
+  console.info(`${req.method} request received for all notes`);
+
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for submitting new note
+note.post('/', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add new note`);
+
+  // Destructuring for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting new note');
+  }
+});
+
+module.exports = note;
